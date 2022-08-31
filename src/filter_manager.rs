acfd888e72603db033ccd62a5f5d2f674d92432f
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2022-08-30 21:34:00 -0500

    Extended documnetation for the filter_manager module (#61)

--- src/filter_manager.rs
+++ src/filter_manager.rs
@@ -1,4 +1,9 @@
-//! Module containing utilities to manipulate the filters saved under the .trane/filters directory.
+//! Contains utilities to use filters saved by the user.
+//!
+//! Trane's default mode for scheduling exercises is to traverse the entire graph. Sometimes,
+//! students want to only schedule exercises from a subset of the graph. This module allows them to
+//! re-use filters they have previously saved.
+
 #[cfg(test)]
 mod test;
 
@@ -8,7 +13,8 @@ use anyhow::Result;
 
 use crate::data::filter::NamedFilter;
 
-/// Contains functions to manage saved filters.
+/// A trait with functions to manage saved filters. Each filter is given a unique name to use as an
+/// identifier and contains a `UnitFilter`.
 pub trait FilterManager {
     /// Gets the filter with the given ID.
     fn get_filter(&self, id: &str) -> Option<NamedFilter>;
@@ -17,14 +23,14 @@ pub trait FilterManager {
     fn list_filters(&self) -> Vec<(String, String)>;
 }
 
-/// An implementation of FilterManager backed by the local filesystem.
+/// An implementation of `FilterManager` backed by the local file system.
 pub(crate) struct LocalFilterManager {
     /// A map of filter IDs to filters.
     filters: HashMap<String, NamedFilter>,
 }
 
 impl LocalFilterManager {
-    /// Scans all NamedFilters in the given directory and returns a map of filters.
+    /// Scans all `NamedFilters` in the given directory and returns a map of filters.
     fn scan_filters(filter_directory: &str) -> Result<HashMap<String, NamedFilter>> {
         let mut filters = HashMap::new();
         for entry in std::fs::read_dir(filter_directory)? {
@@ -36,7 +42,7 @@ impl LocalFilterManager {
         Ok(filters)
     }
 
-    /// Creates a new LocalFilterManager.
+    /// Creates a new `LocalFilterManager`.
     pub fn new(filter_directory: &str) -> Result<LocalFilterManager> {
         Ok(LocalFilterManager {
             filters: LocalFilterManager::scan_filters(filter_directory)?,
