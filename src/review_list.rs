503399db699f4ecbcaab28d0e567993622d9b101
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2022-08-31 19:58:49 -0500

    Move tests to source code files (#62)

--- src/review_list.rs
+++ src/review_list.rs
@@ -4,9 +4,6 @@
 //! add them to the review list. The scheduler implements a special mode that will only schedule
 //! exercises from the units in the review list.
 
-#[cfg(test)]
-mod test;
-
 use anyhow::{Context, Result};
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
@@ -109,5 +106,43 @@ impl ReviewList for ReviewListDB {
             entries.push(Ustr::from(&unit_id));
         }
         Ok(entries)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+    use r2d2_sqlite::SqliteConnectionManager;
+    use ustr::Ustr;
+
+    use super::{ReviewList, ReviewListDB};
+
+    fn new_test_review_list() -> Result<Box<dyn ReviewList>> {
+        let connection_manager = SqliteConnectionManager::memory();
+        let review_list = ReviewListDB::new(connection_manager)?;
+        Ok(Box::new(review_list))
+    }
+
+    #[test]
+    fn add_and_remove_from_review_list() -> Result<()> {
+        let mut review_list = new_test_review_list()?;
+
+        let unit_id = Ustr::from("unit_id");
+        let unit_id2 = Ustr::from("unit_id2");
+        review_list.add_to_review_list(&unit_id)?;
+        review_list.add_to_review_list(&unit_id)?;
+        review_list.add_to_review_list(&unit_id2)?;
+
+        let entries = review_list.all_review_list_entries()?;
+        assert_eq!(entries.len(), 2);
+        assert!(entries.contains(&unit_id));
+        assert!(entries.contains(&unit_id2));
+
+        review_list.remove_from_review_list(&unit_id)?;
+        let entries = review_list.all_review_list_entries()?;
+        assert_eq!(entries.len(), 1);
+        assert!(!entries.contains(&unit_id));
+        assert!(entries.contains(&unit_id2));
+        Ok(())
     }
 }
