e58066dbb9bf33c743d4046db715717394753ac9
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2022-11-20 17:40:55 -0800

    Remove line exclusion from code coverage

--- src/data/filter.rs
+++ src/data/filter.rs
@@ -128,7 +128,7 @@ pub struct MetadataFilter {
 
 /// A filter on a course or lesson manifest.
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub enum UnitFilter { // grcov-excl-line: Code coverage report for this line is flaky.
+pub enum UnitFilter {
     /// A filter to show exercises belonging to the given courses.
     CourseFilter {
         /// The IDs of the courses to filter.
