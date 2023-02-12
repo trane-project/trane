a9e5183270faab1e5b0214555b92016f9c4017b7
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-02-12 14:03:07 -0800

    Fix code coverage

--- src/scheduler/filter.rs
+++ src/scheduler/filter.rs
@@ -59,13 +59,15 @@ impl CandidateFilter {
     fn select_candidates(
         candidates: Vec<Candidate>,
         num_to_select: usize,
-    ) -> Result<(Vec<Candidate>, Vec<Candidate>)> {
+    ) -> (Vec<Candidate>, Vec<Candidate>) {
         // Return the list if there are fewer candidates than the number to select.
         if candidates.len() <= num_to_select {
-            return Ok((candidates, vec![]));
+            return (candidates, vec![]);
         }
 
         // Otherwise, assign a weight to each candidate and perform a weighted random selection.
+        // Safe to unwrap the result, as this function panics if `num_to_select` is greater than the
+        // size of `candidates`, but that is checked above.
         let mut rng = thread_rng();
         let selected: Vec<Candidate> = candidates
             .choose_multiple_weighted(&mut rng, num_to_select, |c| {
@@ -80,7 +82,8 @@ impl CandidateFilter {
                 // weight.
                 weight += (10.0 - c.frequency).max(0.0);
                 weight
-            })?
+            })
+            .unwrap()
             .cloned()
             .collect();
         let selected_ids: UstrSet = selected.iter().map(|c| c.exercise_id).collect();
@@ -92,7 +95,7 @@ impl CandidateFilter {
             .cloned()
             .collect();
 
-        Ok((selected, remainder))
+        (selected, remainder)
     }
 
     /// Fills up the lists of final candidates with the values from remainder if there are not
@@ -101,17 +104,16 @@ impl CandidateFilter {
         batch_size: usize,
         final_candidates: &mut Vec<Candidate>,
         remainder: Vec<Candidate>,
-    ) -> Result<()> {
+    ) {
         // The batch is already full, so there's nothing to do.
         if final_candidates.len() >= batch_size {
-            return Ok(());
+            return;
         }
 
         // Otherwise, select as many candidates as possible from the remainder.
         let num_remainder = batch_size - final_candidates.len();
-        let (remainder_candidates, _) = Self::select_candidates(remainder, num_remainder)?;
+        let (remainder_candidates, _) = Self::select_candidates(remainder, num_remainder);
         final_candidates.extend(remainder_candidates);
-        Ok(())
     }
 
     /// Takes a list of candidates and returns a vector of tuples of exercises IDs and manifests.
@@ -157,23 +159,23 @@ impl CandidateFilter {
         let mut final_candidates = Vec::with_capacity(options.batch_size);
         let num_mastered = (batch_size_float * options.mastered_window_opts.percentage) as usize;
         let (mastered_selected, mastered_remainder) =
-            Self::select_candidates(mastered_candidates, num_mastered)?;
+            Self::select_candidates(mastered_candidates, num_mastered);
         final_candidates.extend(mastered_selected);
 
         // Add elements from the easy window.
         let num_easy = (batch_size_float * options.easy_window_opts.percentage) as usize;
-        let (easy_selected, easy_remainder) = Self::select_candidates(easy_candidates, num_easy)?;
+        let (easy_selected, easy_remainder) = Self::select_candidates(easy_candidates, num_easy);
         final_candidates.extend(easy_selected);
 
         // Add elements from the current window.
         let num_current = (batch_size_float * options.current_window_opts.percentage) as usize;
         let (current_selected, current_remainder) =
-            Self::select_candidates(current_candidates, num_current)?;
+            Self::select_candidates(current_candidates, num_current);
         final_candidates.extend(current_selected);
 
         // For the target window, add as many candidates as possible to fill the batch.
         let remainder = options.batch_size - final_candidates.len();
-        let (target_selected, _) = Self::select_candidates(target_candidates, remainder)?;
+        let (target_selected, _) = Self::select_candidates(target_candidates, remainder);
         final_candidates.extend(target_selected);
 
         // Go through the remainders in ascending order of difficulty and add them to the list of
@@ -182,9 +184,9 @@ impl CandidateFilter {
             options.batch_size,
             &mut final_candidates,
             mastered_remainder,
-        )?;
-        Self::add_remainder(options.batch_size, &mut final_candidates, easy_remainder)?;
-        Self::add_remainder(options.batch_size, &mut final_candidates, current_remainder)?;
+        );
+        Self::add_remainder(options.batch_size, &mut final_candidates, easy_remainder);
+        Self::add_remainder(options.batch_size, &mut final_candidates, current_remainder);
 
         // Convert the list of candidates into a list of tuples of exercise IDs and manifests.
         self.candidates_to_exercises(final_candidates)
