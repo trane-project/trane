a317438b6b28dc944289a828b2eb7fa4ea844a82
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-06-30 21:56:37 -0700

    Return proper errors in repository_manager module

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 //! Contains the errors returned by Trane.
 
-use std::path::PathBuf;
+use std::{io, path::PathBuf};
 
 use thiserror::Error;
 use ustr::Ustr;
@@ -46,8 +46,8 @@ pub enum RepositoryError {
     #[error("failed to clone repository with URL {0}: {1}")]
     CloneRepository(String, #[source] git2::Error),
 
-    #[error("failed to create repository metadata directory .trane/repositories: {0}")]
-    InvalidMetadataDirectory(#[source] std::io::Error),
+    #[error("the repository cannot be copied to {0}: {1}")]
+    CopyRepository(PathBuf, #[source] fs_extra::error::Error),
 
     #[error("another repository with ID {0} already exists")]
     DuplicateRepository(String),
@@ -55,20 +55,23 @@ pub enum RepositoryError {
     #[error("the download directory {0} is invalid or cannot be accessed: {1}")]
     InvalidDownloadDirectory(PathBuf, #[source] std::io::Error),
 
-    #[error("the repository cannot be copied to {0}: {1}")]
-    CopyRepository(PathBuf, #[source] fs_extra::error::Error),
-
-    #[error("the repository at {0} is invalid: {1}")]
-    InvalidRepository(PathBuf, #[source] git2::Error),
+    #[error("failed to create repository metadata directory .trane/repositories: {0}")]
+    InvalidMetadataDirectory(#[source] std::io::Error),
 
     #[error("the repository metadata file at {0} cannot be accessed: {1}")]
     InvalidMetadataFile(PathBuf, #[source] std::io::Error),
+
+    #[error("the repository at {0} is invalid: {1}")]
+    InvalidRepository(PathBuf, #[source] git2::Error),
 
     #[error("the repository metadata at {0} is invalid: {1}")]
     InvalidRepositoryMetadata(PathBuf, #[source] serde_json::Error),
 
     #[error("repository with URL {0} has an invalid URL")]
     InvalidRepositoryURL(String),
+
+    #[error("error creating temp directory: {0}")]
+    TempDir(#[source] io::Error),
 
     #[error("cannot find repository with ID {0}")]
     UnknownRepository(String),
