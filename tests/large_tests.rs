5db3235587479d89056e6e7881e537f93eff64d5
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-07-01 23:33:37 -0700

    set constant size in benchmark

--- tests/large_tests.rs
+++ tests/large_tests.rs
@@ -17,10 +17,10 @@ fn all_exercises_scheduled_random() -> Result<()> {
     let temp_dir = TempDir::new()?;
     let random_library = RandomCourseLibrary {
         num_courses: 50,
-        course_dependencies_range: (0, 5),
-        lessons_per_course_range: (0, 5),
-        lesson_dependencies_range: (0, 5),
-        exercises_per_lesson_range: (0, 20),
+        course_dependencies_range: (5, 5),
+        lessons_per_course_range: (5, 5),
+        lesson_dependencies_range: (5, 5),
+        exercises_per_lesson_range: (10, 10),
     }
     .generate_library();
     let mut trane = init_test_simulation(&temp_dir.path().to_path_buf(), &random_library)?;
