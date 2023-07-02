b8eb746de3a9bf835450b854ef40fb6f06b0431b
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-07-02 16:28:31 -0700

    Exclude error file from coverage

--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,9 @@ use ustr::Ustr;
 
 use crate::data::UnitType;
 
+// grcov-excl-start: This file contains only error definitions so it's safe to exclude it from the
+// code coverage report.
+
 /// An error returned when dealing with the blacklist.
 #[derive(Debug, Error)]
 #[allow(missing_docs)]
