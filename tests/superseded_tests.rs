c4d57cf7635c1da3b906f4efc0aa00c3c2e3a974
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-10-03 18:12:04 -0700

    add test that verifies the bug

--- tests/superseded_tests.rs
+++ tests/superseded_tests.rs
@@ -8,7 +8,13 @@ use std::collections::BTreeMap;
 use anyhow::{Ok, Result};
 use lazy_static::lazy_static;
 use tempfile::TempDir;
-use trane::{data::MasteryScore, testutil::*};
+use trane::{
+    data::{
+        filter::{ExerciseFilter, UnitFilter},
+        MasteryScore,
+    },
+    testutil::*,
+};
 
 lazy_static! {
     /// A simple set of courses to verify that superseded courses and lessons are dealt with
@@ -177,6 +183,21 @@ lazy_static! {
                     id: TestId(6, Some(2), None),
                     dependencies: vec![TestId(6, Some(1), None)],
                     superseded: vec![TestId(6, Some(1), None)],
+                    metadata: BTreeMap::default(),
+                    num_exercises: 10,
+                },
+            ],
+        },
+        TestCourse {
+            id: TestId(7, None, None),
+            dependencies: vec![TestId(6, None, None)],
+            superseded: vec![],
+            metadata: BTreeMap::default(),
+            lessons: vec![
+                TestLesson {
+                    id: TestId(7, Some(0), None),
+                    dependencies: vec![],
+                    superseded: vec![],
                     metadata: BTreeMap::default(),
                     num_exercises: 10,
                 },
@@ -526,6 +547,100 @@ fn scheduler_respects_superseded_lesson_chain() -> Result<()> {
     for exercise_id in &exercise_ids {
         let exercise_ustr = exercise_id.to_ustr();
         if exercise_id.exercise_in_lesson(&superseded_lesson_ids[0]) {
+            assert!(
+                simulation.answer_history.contains_key(&exercise_ustr),
+                "exercise {:?} should have been scheduled",
+                exercise_id
+            );
+            assert_simulation_scores(&exercise_ustr, &trane, &simulation.answer_history)?;
+        }
+    }
+    Ok(())
+}
+
+/// Verifies that the superseded courses are dealt with correctly during scheduling.
+#[test]
+fn scheduler_ignores_superseded_exercises() -> Result<()> {
+    // Initialize test course library.
+    let temp_dir = TempDir::new()?;
+    let mut trane = init_test_simulation(&temp_dir.path(), &LIBRARY)?;
+
+    // Run the simulation, filtering only the  exercises in the superseded lessons and giving them a
+    // score of 1. This will have the effect of bringing down the average score of all athe
+    // exercises in the course.
+    let superseded_lesson_ids = [TestId(6, Some(0), None), TestId(6, Some(1), None)];
+    let mut simulation = TraneSimulation::new(1000, Box::new(|_| Some(MasteryScore::One)));
+    simulation.run_simulation(
+        &mut trane,
+        &vec![],
+        Some(ExerciseFilter::UnitFilter(UnitFilter::LessonFilter {
+            lesson_ids: superseded_lesson_ids
+                .iter()
+                .map(|id| id.to_ustr())
+                .collect(),
+        })),
+    )?;
+
+    // Verify that all the exercises in the superseded lessons were scheduled.
+    let exercise_ids = all_test_exercises(&LIBRARY);
+    for exercise_id in &exercise_ids {
+        let exercise_ustr = exercise_id.to_ustr();
+        if superseded_lesson_ids
+            .iter()
+            .any(|id| exercise_id.exercise_in_lesson(id))
+        {
+            assert!(
+                simulation.answer_history.contains_key(&exercise_ustr),
+                "exercise {:?} should have been scheduled",
+                exercise_id
+            );
+            assert_simulation_scores(&exercise_ustr, &trane, &simulation.answer_history)?;
+        }
+    }
+
+    // Run the simulation, filtering only the  exercises in the superseding lesson and giving them a
+    // score of four. This will have the effect of bringing up the average score of the course and
+    // ensuring the previous lessons are superseded.
+    let superseding_lesson_ids = vec![TestId(6, Some(2), None)];
+    let mut simulation = TraneSimulation::new(1000, Box::new(|_| Some(MasteryScore::Four)));
+    simulation.run_simulation(
+        &mut trane,
+        &vec![],
+        Some(ExerciseFilter::UnitFilter(UnitFilter::LessonFilter {
+            lesson_ids: superseding_lesson_ids
+                .iter()
+                .map(|id| id.to_ustr())
+                .collect(),
+        })),
+    )?;
+
+    // Verify that all the exercises in the superseding lessons were scheduled.
+    let exercise_ids = all_test_exercises(&LIBRARY);
+    for exercise_id in &exercise_ids {
+        let exercise_ustr = exercise_id.to_ustr();
+        if superseding_lesson_ids
+            .iter()
+            .any(|id| exercise_id.exercise_in_lesson(id))
+        {
+            assert!(
+                simulation.answer_history.contains_key(&exercise_ustr),
+                "exercise {:?} should have been scheduled",
+                exercise_id
+            );
+            assert_simulation_scores(&exercise_ustr, &trane, &simulation.answer_history)?;
+        }
+    }
+
+    // Run the simulation again, giving a score of 4 to all exercises. If the superseded exercises
+    // were ignored correctly, the dependent course should have been scheduled. If they were not
+    // ignored correctly, the previous steps ensured the average score of the course is too low to
+    // consider the course as mastered.
+    let mut simulation = TraneSimulation::new(1000, Box::new(|_| Some(MasteryScore::Four)));
+    let dependant_course = TestId(7, None, None);
+    simulation.run_simulation(&mut trane, &vec![], None)?;
+    for exercise_id in &exercise_ids {
+        let exercise_ustr = exercise_id.to_ustr();
+        if exercise_id.exercise_in_course(&dependant_course) {
             assert!(
                 simulation.answer_history.contains_key(&exercise_ustr),
                 "exercise {:?} should have been scheduled",
